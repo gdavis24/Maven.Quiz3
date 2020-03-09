68fc5e7f1f7da06ae8c9a4111fb00b5f25fcace2
Gregory Davis <gdavis@zipcodes-MacBook-Pro-2.local>
2020-03-09 11:41:10 -0400

    most of pig latin working

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -1,14 +1,23 @@
 package rocks.zipcode.io.quiz3.fundamentals;
 
+import static rocks.zipcode.io.quiz3.fundamentals.VowelUtils.isVowel;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class PigLatinGenerator {
     public String translate(String str) {
 //        VowelUtils vowel = new VowelUtils();
-        if (VowelUtils.startsWithVowel(str) == true){
+        String[] strArray = str.split(" ");
+        if (VowelUtils.startsWithVowel(str) == true) {
             return str + "way";
         }
-        return null;
+        for (int i = 0; i < str.length(); i++) {
+            if (isVowel(str.toCharArray()[i])) {
+
+                return str.substring(i, str.length()) + str.substring(0, i) + "ay";
+            }
+        }
+        return str + "ay";
     }
 }
