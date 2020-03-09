759be41bd95df81729280765439e1451ee465b86
Gregory Davis <gdavis@zipcodes-MacBook-Pro-2.local>
2020-03-09 10:28:34 -0400

    first tests passing

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 public class StringUtils {
     public static String capitalizeNthCharacter(String str, Integer indexToCapitalize) {
-        return null;
+        return str.substring(0,indexToCapitalize) + str.substring(indexToCapitalize, indexToCapitalize + 1).toUpperCase() + str.substring(indexToCapitalize + 1);
     }
 
     public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
--- src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
+++ src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
@@ -4,7 +4,15 @@ package rocks.zipcode.io.quiz3.objectorientation.enums;
  * @author leon on 09/12/2018.
  */
 public enum RockPaperScissorHandSign {
-    ADD_ENUMERATIONS_HERE;
+    ROCK("ROCK"),
+    PAPER("PAPER"),
+    SCISSOR("SCISSOR");
+
+    private final String value;
+
+    RockPaperScissorHandSign(String value) {
+        this.value = value;
+    }
 
     public RockPaperScissorHandSign getWinner() {
         return null;
