30ce6dc2a21e2960e1c927f3736cd4831384bfc6
Gregory Davis <gdavis@zipcodes-MacBook-Pro-2.local>
2020-03-09 10:47:50 -0400

    next string tests passing

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
@@ -8,21 +8,38 @@ import java.util.List;
  */
 public class StringUtils {
     public static String capitalizeNthCharacter(String str, Integer indexToCapitalize) {
-        return str.substring(0,indexToCapitalize) + str.substring(indexToCapitalize, indexToCapitalize + 1).toUpperCase() + str.substring(indexToCapitalize + 1);
+        return str.substring(0, indexToCapitalize) + str.substring(indexToCapitalize, indexToCapitalize + 1).toUpperCase() + str.substring(indexToCapitalize + 1);
     }
 
     public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
-        if (baseString.charAt(indexOfString) == characterToCheckFor.charValue()){
+        if (baseString.charAt(indexOfString) == characterToCheckFor.charValue()) {
             return true;
         }
         return false;
     }
 
     public static String[] getAllSubStrings(String string) {
-        return null;
+        String[] listingSubs = new String[string.length()];
+        String temp;
+        int k = 0;
+        for (int i = 0; i < string.length() - 1; i++) {
+            for (int j = i + 1; j < string.length() - 2; j++) {
+                temp = string.substring(i, i + j);
+                listingSubs[k++] = temp;
+            }
+
+        }
+        return listingSubs;
     }
 
-    public static Integer getNumberOfSubStrings(String input){
-        return null;
+    public static Integer getNumberOfSubStrings(String input) {
+        String temp;
+        int count = -1;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i; j < input.length(); j++) {
+                count++;
+            }
+        }
+        return count;
     }
 }
