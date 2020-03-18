7e15f0088b5da8d90e1dabf27c8a5b921d18f7a0
Greg Davis <jrobley3@hotmail.com>
2020-03-18 12:17:06 -0400

    finished tic tac toe

--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -4,36 +4,69 @@ package rocks.zipcode.io.quiz3.arrays;
  * @author leon on 09/12/2018.
  */
 public class TicTacToe {
-    private String[][] matrix;
+    private String[][] board;
 
     public TicTacToe(String[][] board) {
-        this.matrix = board;
+        this.board = board;
     }
 
     public TicTacToe() {
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        return this.board[value];
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+        String[] column = new String[3];
+        for (int i = 0; i < 3; i++) {
+            column[i] = this.board[i][value];
+        }
+        return column;
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
-        return null;
+        if (getRow(rowIndex)[0] == "X" && getRow(rowIndex)[1] == "X" && getRow(rowIndex)[2] == "X" ||
+                getRow(rowIndex)[0] == "O" && getRow(rowIndex)[1] == "O" && getRow(rowIndex)[2] == "O") {
+            return true;
+        }
+        return false;
     }
 
     public Boolean isColumnHomogeneous(Integer columnIndex) {
-        return null;
+        if (getColumn(columnIndex)[0] == "X" && getColumn(columnIndex)[1] == "X" && getColumn(columnIndex)[2] == "X" ||
+                getColumn(columnIndex)[0] == "O" && getColumn(columnIndex)[1] == "O" && getColumn(columnIndex)[2] == "O") {
+            return true;
+        }
+        return false;
     }
 
+    public Boolean isDiagonalHomogeneous() {
+        if (board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X" ||
+                board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X" ||
+                board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O" ||
+                board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O"){
+            return true;
+        }
+        return false;
+    }
     public String getWinner() {
-        return null;
+        String winner = "";
+        for (int i = 0; i < 3; i++) {
+            if (isRowHomogenous(i)){
+                winner = getRow(i)[0];
+            }
+            if (isColumnHomogeneous(i)){
+                winner = getColumn(i)[0];
+            }
+            if (isDiagonalHomogeneous()){
+                winner = getColumn(1)[1];
+            }
+        }
+        return winner;
     }
 
     public String[][] getBoard() {
-        return matrix;
+        return board;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -7,15 +7,43 @@ import java.util.stream.Stream;
  */
 public class WaveGenerator {
     public static String[] wave(String str) {
-        String[] result = new String[0];
+//        String[] result = new String[0];
+//
+//        String waveString = str.toLowerCase();
+//        for (int i = 0; i < waveString.length(); i++) {
+//            String newString = waveString.substring(0, i) + waveString.toUpperCase().charAt(i) + waveString.substring(i + 1, waveString.length());
+//            System.out.println(newString);
+//            String[] second = newString.split(" ");
+//            result = newString.split(" ");
+//            result[i] = newString;
+//        }
 
-        String waveString = str.toLowerCase();
-        for (int i = 0; i < waveString.length(); i++) {
-            String newString = waveString.substring(0, i) + waveString.toUpperCase().charAt(i) + waveString.substring(i + 1, waveString.length());
-            System.out.println(newString);
-            String[] second = newString.split(" ");
-            result = newString.split(" ");
+        int count = 0;
+        for (char c : str.toCharArray()) {
+            Character ca = c;
+            if (Character.isAlphabetic(ca)) {
+                count++;
+            }
         }
-        return result;
+        String[] capped = new String[count];
+        for (int i = 0; i < count; i++) {
+            String toAdd = "";
+            int index = 0;
+            for (char c : str.toCharArray()) {
+                Character ca = c;
+                if (Character.isAlphabetic(ca)) {
+                    if (index == i) {
+                        toAdd += Character.toUpperCase(ca);
+                    } else {
+                        toAdd += Character.toLowerCase(ca);
+                    }
+                    index++;
+                } else {
+                    toAdd += ca;
+                }
+            }
+            capped[i] = toAdd;
+        }
+        return capped;
     }
 }
